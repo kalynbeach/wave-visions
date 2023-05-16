556af72193c692605b297d263aae0f464152171f
kalynbeach <kalynbeach@gmail.com>
2023-05-16 16:13:38 -0700

    make useMediaDevices hook get permission for MediaDevice labels

--- src/app/devices/useMediaDevices.ts
+++ src/app/devices/useMediaDevices.ts
@@ -7,7 +7,10 @@ export default function useMediaDevices() {
 
   useEffect(() => {
     async function fetchMediaDevices() {
+      // Get user permission to access media device labels
+      await navigator.mediaDevices.getUserMedia({ audio: true })
       const devices = await navigator.mediaDevices.enumerateDevices()
+      console.log('[MediaDevices] devices: ', devices)
       setMediaDevices(devices)
     }
     fetchMediaDevices()
