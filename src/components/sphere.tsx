a020d9d8817bcab9eb6b3d84a12e9b4d27ab7e37
kalynbeach <kalynbeach@gmail.com>
2023-11-16 17:17:08 -0800

    update sphere mesh animations

--- src/components/sphere.tsx
+++ src/components/sphere.tsx
@@ -10,34 +10,53 @@ type Props = ThreeElements['mesh'] & {
 
 export default function Sphere(props: Props) {
   const ref = useRef<THREE.Mesh>(null!)
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+  // const [hovered, hover] = useState(false)
+  // const [clicked, click] = useState(false)
   
-  const _scale = props.volume ? Math.max(props.volume / 70, 1) : 1
-  const _rotation = props.volume ? props.volume / 90 : 0.03
-  const _rotationX = _rotation * 0.001
-  const _rotationY = _rotation * 0.03
-  const _rotationZ = _rotation * 0.0
-
+  // useFrame((state, delta) => {
+  //   const _rotation = props.volume ? props.volume / 90 : 0.03
+  //   const _rotationX = _rotation * 0.001
+  //   const _rotationY = _rotation * 0.03
+  //   const _rotationZ = _rotation * 0.0
+  //   ref.current.rotation.y += _rotationY
+  //   if (props.volume && props.volume > 60) {
+  //     ref.current.rotation.z += _rotationZ
+  //   }
+  //   if (props.volume && props.volume > 90) {
+  //     ref.current.rotation.x += _rotationX
+  //   }
+  // })
+  
   useFrame((state, delta) => {
-    ref.current.rotation.y += _rotationY
-    if (props.volume && props.volume > 60) {
-      ref.current.rotation.z += _rotationZ
-    }
-    if (props.volume && props.volume > 90) {
-      ref.current.rotation.x += _rotationX
-    }
+    ref.current.rotation.x = 1.57
 
+    if (props.volume) {
+      const _scale = Math.max(props.volume * 0.014, 1)
+      ref.current.scale.x = _scale
+      ref.current.scale.y = _scale
+      ref.current.scale.z = _scale
+      ref.current.rotation.y -= _scale * 0.003
+      // if (props.volume > 90) {
+      //   ref.current.rotation.z -= _scale * 0.0003
+      // }
+    } else {
+      ref.current.scale.x = 1
+      ref.current.scale.y = 1
+      ref.current.scale.z = 1
+      ref.current.rotation.y -= 0.003
+      // ref.current.rotation.z += 0.003
+    }
   })
-
+  
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={_scale}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      // scale={3}
+      // onClick={(event) => click(!clicked)}
+      // onPointerOver={(event) => hover(true)}
+      // onPointerOut={(event) => hover(false)}
+    >
       <sphereGeometry args={[1, 16, 16]} />
       <meshStandardMaterial color={'#1AE803'} wireframe />
     </mesh>
