4e3f7d79ea048a81a6c540c31693979c4fd6e17d
kalynbeach <kalynbeach@gmail.com>
2023-12-22 18:34:35 -0800

    update audio-info styles

--- src/components/audio-info.tsx
+++ src/components/audio-info.tsx
@@ -7,7 +7,7 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function AudioInfo() {
   const [processor] = useAudioProcessor();
-  const [isMinimized, setIsMinimized] = useState(false);
+  const [isMinimized, setIsMinimized] = useState(true);
 
   function toggleMinimize() {
     setIsMinimized(!isMinimized);
@@ -27,22 +27,20 @@ export default function AudioInfo() {
           {isMinimized ? <PlusIcon /> : <MinusIcon />}
         </Button>
       </div>
-      { !isMinimized && (
-        <div className="w-full flex flex-col justify-between gap-3">
-          <div className="flex flex-row justify-between gap-2">
-            <p className="text-xs font-mono font-medium">volume</p>
-            <p className="text-sm font-mono">{processor.volume}</p>
-          </div>
-          <div className="flex flex-col divide-y divide-neutral-900">
-            {processor.frequencies && Object.entries(processor.frequencies).map((frequency, index) => (
-              <div key={index} className="py-1 flex flex-row justify-between items-center">
-                <span className="text-xs font-mono font-medium">{frequency[0]}</span>
-                <span className="text-sm font-mono">{frequency[1]}</span>
-              </div>
-            ))}
-          </div>
+      <div className={`${isMinimized ? "hidden" : "h-fit w-full flex flex-col justify-between gap-3"}`}>
+        <div className="flex flex-row justify-between">
+          <p className="text-xs font-mono font-medium">volume</p>
+          <p className="text-sm font-mono">{processor.volume}</p>
         </div>
-      )}
+        <div className="flex flex-col divide-y divide-neutral-900">
+          {processor.frequencies && Object.entries(processor.frequencies).map((frequency, index) => (
+            <div key={index} className="py-1 flex flex-row justify-between items-center">
+              <span className="text-xs font-mono font-medium">{frequency[0]}</span>
+              <span className="text-sm font-mono">{frequency[1]}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
