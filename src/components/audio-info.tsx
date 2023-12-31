c9f03f56226d27fae6b7b6261d83a04a9f97c936
kalynbeach <kalynbeach@gmail.com>
2023-12-30 21:03:48 -0800

    update audio-info and vision-controls component styles

--- src/components/audio-info.tsx
+++ src/components/audio-info.tsx
@@ -20,12 +20,12 @@ export default function AudioInfo() {
   return (
     <div className="col-span-2 md:col-span-1 row-start-5 sm:row-end-7 sm:self-end z-50 md:w-64 h-fit m-2 p-3 sm:p-4 flex flex-col gap-4 bg-background border rounded-sm">
       <div className="w-full flex flex-row items-center justify-between">
-        <span className="font-mono font-bold text-sm">Audio Info</span>
+        <span className="text-lg font-bold">AudioInfo</span>
         <Button
           onClick={toggleMinimize}
           size="icon"
           variant="outline"
-          className="border border-neutral-900 rounded-sm"
+          className="h-7 w-7 border rounded-sm"
           aria-label="Minimize Audio Info Button"
         >
           {isMinimized ? <PlusIcon /> : <MinusIcon />}
@@ -33,13 +33,13 @@ export default function AudioInfo() {
       </div>
       <div className={`${isMinimized ? "hidden" : "h-fit w-full flex flex-col justify-between gap-3"}`}>
         <div className="flex flex-row justify-between">
-          <p className="text-xs font-mono font-medium">volume</p>
+          <p className="text-sm font-mono font-medium">volume</p>
           <p className="text-sm font-mono">{processor.volume}</p>
         </div>
         <div className="flex flex-col divide-y divide-neutral-900">
           {processor.frequencies && Object.entries(processor.frequencies).map((frequency, index) => (
-            <div key={index} className="py-1 flex flex-row justify-between items-center">
-              <span className="text-xs font-mono font-medium">{frequency[0]}</span>
+            <div key={index} className="py-2 flex flex-row justify-between items-center">
+              <span className="text-sm font-mono font-medium">{frequency[0]}</span>
               <span className="text-sm font-mono">{frequency[1]}</span>
             </div>
           ))}
--- src/components/vision-controls.tsx
+++ src/components/vision-controls.tsx
@@ -18,14 +18,14 @@ export default function VisionControls() {
   if (!waveVisions.showVisionControls) return null;
 
   return (
-    <div className="vision-controls col-span-3 sm:col-end-7 row-end-7 self-end z-50 h-fit m-2 p-3 sm:p-4 flex flex-col gap-3 bg-background border rounded-sm">
+    <div className="vision-controls col-span-3 sm:col-span-2 sm:col-end-7 row-end-7 self-end z-50 h-fit m-2 p-3 sm:p-4 flex flex-col gap-3 bg-background border rounded-sm">
       <div className="w-full flex flex-row items-center justify-between">
-        <span className="font-mono font-bold text-sm">Vision Controls</span>
+        <span className="text-lg font-bold">VisionControls</span>
         <Button
           onClick={toggleMinimize}
           size="icon"
           variant="outline"
-          className="border border-neutral-900 rounded-sm"
+          className="h-7 w-7 border rounded-sm"
           aria-label="Minimize Volume Controls Button"
         >
           {isMinimized ? <PlusIcon /> : <MinusIcon />}
@@ -33,36 +33,36 @@ export default function VisionControls() {
       </div>
       <div className={`${isMinimized ? "hidden" : "h-fit w-full flex flex-col justify-between gap-3"}`}>
         <div className="flex flex-row justify-between">
-          <span className="text-xs font-mono">agility</span>
+          <span className="basis-1/4 text-sm font-mono font-medium">agility</span>
           <input
             type="range"
             min="0"
             max="100"
             value={vision.agility}
             onChange={e => setVision(prevState => ({ ...prevState, agility: parseInt(e.target.value) }))}
-            className="accent-[#1AE803]"
+            className="flex-1 accent-[#1AE803]"
           />
         </div>
         <div className="flex flex-row justify-between">
-          <span className="text-xs font-mono">intellect</span>
+          <span className="basis-1/4 text-sm font-mono font-medium">intellect</span>
           <input
             type="range"
             min="0"
             max="100"
             value={vision.intellect}
             onChange={e => setVision(prevState => ({ ...prevState, intellect: parseInt(e.target.value) }))}
-            className="accent-[#1AE803]"
+            className="flex-1 accent-[#1AE803]"
           />
         </div>
         <div className="flex flex-row justify-between">
-          <span className="text-xs font-mono">strength</span>
+          <span className="basis-1/4 text-sm font-mono font-medium">strength</span>
           <input
             type="range"
             min="0"
             max="100"
             value={vision.strength}
             onChange={e => setVision(prevState => ({ ...prevState, strength: parseInt(e.target.value) }))}
-            className="accent-[#1AE803]"
+            className="flex-1 accent-[#1AE803]"
           />
         </div>
       </div>
