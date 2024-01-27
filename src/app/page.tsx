c23271e1060fe0f3e228145658c801c22a3fba13
kalynbeach <kalynbeach@gmail.com>
2024-01-26 18:57:01 -0800

    add audio stream volume effect and Suspense for vision-canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
 import VisionCanvas from "@/components/vision-canvas";
 
 export default function Home() {
   return (
-    <VisionCanvas />
+    // <VisionCanvas />
+    <Suspense fallback={<div className="w-full h-full flex justify-center items-center">Loading...</div>}>
+      <VisionCanvas />
+    </Suspense>
   );
 }
--- src/components/vision-canvas.tsx
+++ src/components/vision-canvas.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useAtom } from "jotai";
 import {
   activeVisionAtom,
   activeAudioDeviceAtom,
   audioStreamAtom,
   audioProcessorAtom,
+  audioVolumeAtom,
 } from "@/lib/store";
 import { AudioProcessor } from "@/lib/audio-processor";
-import { VisionName } from "@/lib/definitions";
+import { AudioFrequencies, VisionName } from "@/lib/definitions";
 import BoxesVision from "./boxes-vision";
 import SphereVision from "./sphere-vision";
 import OscilloscopeVision from "./oscilloscope-vision";
@@ -21,12 +22,17 @@ export default function VisionCanvas({}: Props) {
   const [audioDevice] = useAtom(activeAudioDeviceAtom);
   const [audioStream, setAudioStream] = useAtom(audioStreamAtom);
   const [audioProcessor, setAudioProcessor] = useAtom(audioProcessorAtom);
+  const [audioVolume, setAudioVolume] = useAtom(audioVolumeAtom);
 
+  // TODO: Refactor this thing
   const VisionComponentMap: { [K in VisionName]: JSX.Element } = {
     "Sphere": <SphereVision />,
     "Boxes": <BoxesVision />,
     "Oscilloscope": <OscilloscopeVision />,
   };
+
+  const frequenciesRef = useRef<AudioFrequencies | null>(null);
+  const volumeRef = useRef<number>(0);
 
   // Initialize audio stream from active audio device
   useEffect(() => {
@@ -55,6 +61,34 @@ export default function VisionCanvas({}: Props) {
 
     return () => {};
   }, [audioStream]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // Process audio volume data
+  useEffect(() => {
+    let animationFrameId: number;
+  
+    const processVolume = (processor: AudioProcessor) => {
+      const updateVolume = () => {
+        const volume = processor.getVolume() || 0;
+        // Only update if volume changes significantly
+        if (Math.abs(volume - volumeRef.current) > 0.01) {
+          setAudioVolume(volume)
+          volumeRef.current = volume;
+        }
+        animationFrameId = requestAnimationFrame(updateVolume);
+      }
+      updateVolume();
+    }
+  
+    if (audioProcessor) {
+      processVolume(audioProcessor);
+    }
+  
+    return () => {
+      if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, [audioProcessor]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="vision-canvas w-full h-full">
